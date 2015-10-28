e6dbb499c17d925e3d1ccbdab3b1856d141198e2
neeraj6488 <neeraj6488@gmail.com>
2015-10-28 21:50:36 +0530

    None

---
+++ ModalBootstrap/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModalBootstrap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewLyubomir()
+        {
+            return PartialView("_Lyubomir");
+        }
+
+        [HttpPost]
+        public ActionResult Lyubomir()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ModalBootstrap/Models/DeleteConfirmationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ModalBootstrap.Models
+{
+    public class DeleteConfirmationViewModel
+    {
+        // Action to perform after user confirms "Delete".
+        public string PostDeleteAction { get; set; }
+
+        // [OPTIONAL] Controller to look for Post Delete action.
+        // This controller has implementation of Post Delete action
+        public string PostDeleteController { get; set; }
+
+        // While executing POST Delete action, we need id of entity to delete.
+        public int DeleteEntityId { get; set; }
+
+        // Delete Confirmation dialog header text. For example
+        // For text like "Delete Estimated Effort", Header Text is
+        // "Estimated Effort"
+        public string HeaderText { get; set; }
+    }
+}
